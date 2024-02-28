0c34633107092224421555faf7a3392f090647c4
Wesley Vieira <92549651+VieiraSantosz@users.noreply.github.com>
2024-02-28 17:08:22 -0300

    Delete Python directory

--- Python/orientação-objetos/3.Métodos de Classe/ex2.py
+++
@@ -1,8 +0,0 @@
-# 2. Crie uma classe chamada ContaBancaria com um construtor que aceita os parâmetros titular e saldo. 
-# Inicie o atributo ativo como False por padrão.
-
-class ContaBancaria:
-    def __init__(self, titular, saldo):
-        self.titular    = titular
-        self.saldo      = float(saldo)
-        self.ativo      = False
--- Python/orientação-objetos/5.Herança/ex1 (banco)/agencia.py
+++
@@ -1,7 +0,0 @@
-from banco import Banco
-
-class Agencia(Banco):
-    
-    def __init__(self, nome, endereco, numero):
-        super().__init__(nome, endereco)
-        self._numero = numero    
